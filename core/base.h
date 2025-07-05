a069834972f372ae8ecdf357750cc91799e4f9c8
infopek <10saero01@gmail.com>
2025-07-05 13:00:38 +0200

    Comment out include in base.h

--- core/base.h
+++ core/base.h
@@ -3,9 +3,9 @@
 
 // This file needs to be included first for all MSDFgen sources
 
-#ifndef MSDFGEN_PUBLIC
-#include <msdfgen/msdfgen-config.h>
-#endif
+// #ifndef MSDFGEN_PUBLIC
+// #include <msdfgen/msdfgen-config.h>
+// #endif
 
 #include <cstddef>
 
